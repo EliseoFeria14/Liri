ceb762bc0429ba4463d187d5e8f175b7dabe69c4
EliseoMejia <eliseofmejia@gmail.com>
2017-08-26 21:51:46 -0400

    initial files: key.js, liri.js, package.json, and gitignore

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+	consumer_key: "8F6VgrgYq8EVB7fYCvAKB14Xi",
+	consumer_secret: "7xbZAwntskCfrTH04QODCBcTs7HxmsTocAJMZZeCkffMCLj0CN",
+	access_token_key: "	901483681935708160-cJziXnejQ1kibs9n0V0sj4IzcLn4sAX",
+	access_token_secret: "	tgwVBTFZvmm8fMr6DzgIp8h9OK6MoYxVXwNXM7IPplLXQ"
+}
---
+++ liri.js
@@ -0,0 +1,10 @@
+//requiring the npm's need Twitter, Spotify, fs, request
+var Twitter = require("twitter");
+
+var request = require("request");
+
+var Spotity = require("node-spotify-api");
+
+var fs = require("fs");
+
+var keys = require("./keys.js");
