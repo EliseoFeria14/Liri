c7ae0d85df92451ab8a8914692ca62043b2238e7
EliseoMejia <eliseofmejia@gmail.com>
2017-08-26 22:53:30 -0400

    getting the command line arguments and update to keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,13 @@
+console.log("keys loaded");
+
 exports.twitterKeys = {
 	consumer_key: "8F6VgrgYq8EVB7fYCvAKB14Xi",
 	consumer_secret: "7xbZAwntskCfrTH04QODCBcTs7HxmsTocAJMZZeCkffMCLj0CN",
 	access_token_key: "	901483681935708160-cJziXnejQ1kibs9n0V0sj4IzcLn4sAX",
 	access_token_secret: "	tgwVBTFZvmm8fMr6DzgIp8h9OK6MoYxVXwNXM7IPplLXQ"
-}
+};
+
+exports.spotifyKeys = {
+	client_Id: "c1e346ba4b064e09bb393c97d2660aed",
+	client_Secret: "6be70d5d6c0e4d64b3bfe729ff730ab8"
+};
--- liri.js
+++ liri.js
@@ -8,3 +8,16 @@ var Spotity = require("node-spotify-api");
 var fs = require("fs");
 
 var keys = require("./keys.js");
+
+//grabing the command line arguments
+var action = process.argv[2];
+console.log(action);
+
+//building the search request
+var nodeArg = process.argv;
+var searchRequest = "";
+for (var i =3; i < nodeArg.length; i++) {
+	searchRequest = searchRequest + " " + nodeArg[i];
+}
+//logging search request
+console.log("You are searching: " +searchRequest);
